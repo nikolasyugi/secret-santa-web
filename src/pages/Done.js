d3e0be34da79d2df29111f2240179959397b8e67
nikolasyugi <nikolas.yugi@gmail.com>
2019-12-06 08:36:30 -0300

    implemented done page

--- src/pages/Done.js
+++ src/pages/Done.js
@@ -1,15 +1,22 @@
 import React, { Component } from "react";
-
+import { faChevronLeft } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import "./Done.css";
+import { Link } from "react-router-dom";
 
 class Done extends Component {
 
     render() {
         return (
             <div id="page-wrapper">
+                <Link to="/">
+                    <FontAwesomeIcon icon={faChevronLeft} className="back-chevron"></FontAwesomeIcon>
+                </Link>
                 <h1>Parabéns!</h1>
                 <p>O seu sorteio foi realizado e um e-mail foi enviado aos participantes.</p>
-                <button onClick={this.handleSubmitCode}>Novo Sorteio</button>
+                <Link to="/" className="new-draw-link">
+                    <button onClick={this.handleSubmitCode}>Novo Sorteio</button>
+                </Link>
             </div>
         );
     }
